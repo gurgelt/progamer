5dbfed7d69b6cde9f0cc44647809e18c6f2dbdb7
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-08 09:46:42 -0300

    first commit

---
+++ src/main/java/br/com/fiap/bean/SetupBean.java
@@ -0,0 +1,34 @@
+package br.com.fiap.bean;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.com.fiap.dao.SetupDAO;
+import br.com.fiap.model.Setup;
+
+@Named
+@RequestScoped
+public class SetupBean {
+
+	private Setup setup = new Setup();
+
+	public void save() {
+		System.out.println(this.setup);
+		new SetupDAO().insert(setup);
+	}
+	
+	public List<Setup> getSetups(){
+		return new SetupDAO().list();
+	}
+	
+	public Setup getSetup() {
+		return setup;
+	}
+
+	public void setSetup(Setup setup) {
+		this.setup = setup;
+	}
+	
+}
